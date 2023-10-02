4c238db82dc382c4ac899cf2eee7f441b1487207
Gayun00 <gayunlee11@gmail.com>
2023-10-02 16:51:47 +0900

    Refactor: seperate logic, add post route logic

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 import express from "express";
 import bodyParser from "body-parser";
 
+import { scrapArticle } from "./services/scrapper.js";
+import { extractKeywords } from "./services/openai.js";
+
 const app = express();
 const port = 5000;
 
-// POST 요청을 파싱하기 위해 bodyParser 사용
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// POST 요청을 처리하는 예제
-app.post("/api/post", (req, res) => {
-  // POST 요청에서 전송된 데이터는 req.body에서 사용 가능
+app.post("/article", async (req, res) => {
   const requestData = req.body;
+  console.log("requestData: ", requestData);
+  const { text, title, imageUrl } = await scrapArticle(requestData.url);
+  const keywords = await extractKeywords(title);
 
-  // 여기에 원하는 로직을 추가하십시오.
-  // 이 예제에서는 전송된 데이터를 그대로 응답합니다.
   res.json({
     message: "POST 요청이 성공적으로 처리되었습니다.",
-    data: requestData,
+    data: {
+      text,
+      title,
+      imageUrl,
+      keywords,
+    },
   });
 });
 
-// 서버 시작
 app.listen(port, () => {
   console.log(`서버가 http://localhost:${port} 에서 실행 중입니다.`);
 });
