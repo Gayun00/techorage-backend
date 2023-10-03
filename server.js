87b003e147824997c56d1361cc42920b375f86cb
gygy <67543454+Gayun00@users.noreply.github.com>
2023-10-03 16:33:34 +0900

    Add get api, adding keywords
    * Add: get route
    
    * Add: url schema
    
    * Add: add keywords

--- server.js
+++ server.js
@@ -4,7 +4,13 @@ import bodyParser from "body-parser";
 
 import { scrapArticle } from "./services/scrapper.js";
 import { extractKeywords } from "./services/openai.js";
-import { createArticle, getArticles } from "./services/sanity.js";
+import {
+  createArticle,
+  deleteArticles,
+  deleteKeywords,
+  getArticles,
+  updateKeywords,
+} from "./services/sanity.js";
 
 const app = express();
 const port = 5000;
@@ -18,10 +24,49 @@ app.post("/article", async (req, res) => {
 
   const { text, title, imageUrl } = await scrapArticle(requestData.url);
   const keywords = await extractKeywords(title);
-  await createArticle({ text, title, keywords });
+  await createArticle({
+    text,
+    title,
+    keywords,
+    url: requestData.url,
+    thumbnail: imageUrl,
+  });
+  await updateKeywords(keywords);
 
   res.json({
     message: "요청이 성공적으로 처리되었습니다.",
+  });
+});
+
+app.get("/articles", async (req, res) => {
+  const { token } = req.body;
+
+  const articles = await getArticles({ token });
+
+  res.json({
+    message: "요청이 성공적으로 처리되었습니다.",
+    articles,
+  });
+});
+
+app.post("/test", async (req, res) => {
+  await updateKeywords(["좋은 컴포넌트 설계2", "고민", "Compound component"]);
+  res.json({
+    message: "키워드 업데이트 성공",
+  });
+});
+
+app.delete("/keywords", async (req, res) => {
+  await deleteKeywords();
+  res.json({
+    message: "키워드 삭제 성공",
+  });
+});
+
+app.delete("/articles", async (req, res) => {
+  await deleteArticles();
+  res.json({
+    message: "아티클 삭제 성공",
   });
 });
 
--- services/sanity.js
+++ services/sanity.js
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 export const client = createClient({
-  projectId: process.env.SANITY_PROJECT_TOKEN,
+  projectId: process.env.SANITY_PROJECT_ID,
   dataset: "production",
   useCdn: true,
   apiVersion: "2023-10-02",
@@ -18,11 +18,92 @@ export async function getArticles() {
 }
 
 export async function createArticle(article) {
-  const result = client.createIfNotExists({
+  const result = await client.createIfNotExists({
     _id: uuidv4(),
     _type: "article",
     ...article,
   });
-  console.log(result);
+  console.log(result, article);
   return result;
 }
+
+const checkKeywordExistence = (value) => {
+  const query = `*[_type == "keywords" && keyword == $value]`;
+
+  return client.fetch(query, { value }).then((keyword) => {
+    console.log("value", value, keyword, "keyword");
+    return !!keyword.length;
+  });
+};
+
+// Known issue: 3~4회 반복까지는 existence검사가 정상동작하지 않는 경우 발생
+export const updateKeywords = async (keywords) => {
+  for (const keyword of keywords) {
+    const isExist = await checkKeywordExistence(keyword);
+    console.log(keyword, isExist);
+    if (!isExist) {
+      try {
+        await client
+          .create({
+            _id: uuidv4(),
+            _type: "keywords",
+            keyword,
+            count: 0,
+          })
+          .then((doc) => {
+            console.log("created! New document:", doc);
+          });
+      } catch (err) {
+        console.error("Oh no, creation failed: ", err.message);
+      }
+    }
+  }
+};
+
+// await client
+//   .patch(keyword)
+//   .inc({ count: 1 })
+//   .commit()
+//   .then((updatedBike) => {
+//     console.log("updated! New document:");
+//     console.log(updatedBike);
+//   })
+//   .catch((err) => {
+//     console.error("Oh no, the update failed: ", err.message);
+//   });
+
+export const deleteKeywords = () => {
+  client
+    .delete({ query: '*[_type == "keywords"]' })
+    .then(() => {
+      console.log("The document was deleted");
+    })
+    .catch((err) => {
+      console.error("Delete failed: ", err.message);
+    });
+};
+
+export const deleteArticles = () => {
+  client
+    .delete({ query: '*[_type == "article"]' })
+    .then(() => {
+      console.log("The document was deleted");
+    })
+    .catch((err) => {
+      console.error("Delete failed: ", err.message);
+    });
+};
+
+// 이미지 처리 방식 결정까지 주석처리
+// export async function uploadImage(imageFile) {
+//   return fetch(
+//     `https://${SANITY_PROJECT_ID}.api.sanity.io/v2023-10-02/assets/images/production`,
+//     {
+//       method: "POST",
+//       headers: {
+//         "Content-Type": "image/jpeg",
+//       },
+//       body: imageFile,
+//     }
+//   );
+// }
